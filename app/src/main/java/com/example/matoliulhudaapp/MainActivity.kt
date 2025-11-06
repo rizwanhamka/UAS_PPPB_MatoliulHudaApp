637c07224ce2d83c1a3684f0382f58659ed9de6f
rizwanhamka <ahmadrizwanhamka@mail.ugm.ac.id>
2025-11-06 21:45:08 +0700

    add some ui

--- app/src/main/java/com/example/matoliulhudaapp/MainActivity.kt
+++ app/src/main/java/com/example/matoliulhudaapp/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.matoliulhudaapp.components.NotificationsFragment
+import com.example.matoliulhudaapp.components.ProfileFragment
+import com.example.matoliulhudaapp.components.InputFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_notification -> loadFragment(NotificationsFragment())
+                R.id.nav_profile -> loadFragment(ProfileFragment())
+                R.id.nav_input -> loadFragment(InputFragment())
                 else -> loadFragment(HomeFragment())
             }
             true
